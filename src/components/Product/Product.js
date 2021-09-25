2da4f45a1fafb5fbcd4e28a7c9f68753f896f931
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-09-25 16:17:44 +0600

    dynamic star is added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const { img, name, seller, price, stock, key } = props.product;
+    const { img, name, seller, price, stock, key, star } = props.product;
     return (
         <div className='product'>
             <div>
@@ -13,14 +14,24 @@ const Product = (props) => {
             </div>
 
             <div>
-                <h4 className='product-name'><Link to = {'/product/'+key}>{name}</Link></h4>
+                <h4 className='product-name'><Link to={'/product/' + key}>{name}</Link></h4>
                 <br />
                 <p><small>By : {seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
-                { props.showAddToCart &&
-                    <button onClick = {() => props.HandleAddProduct(props.product)} className = 'main-button'>
-                    <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly
+                >
+                </Rating>
+                <br />
+
+                {props.showAddToCart &&
+                    <button onClick={() => props.HandleAddProduct(props.product)} className='main-button'>
+                        <FontAwesomeIcon icon={faShoppingCart} /> add to cart
                     </button>
                 }
             </div>
